180dbe5776ae7aa5d1c5139af52ffeefe43146d4
Tae Hong Min <taemin1@Taes-MBP.fios-router.home>
2017-11-19 15:39:17 -0500

    part 3 poop

--- serverside/catalog/views.py
+++ serverside/catalog/views.py
@@ -2,16 +2,21 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-from rest_framework.views import APIView
+from rest_framework import generics
 from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
 
-class ProductList(APIView):
-    def get(self, request, format=None):
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
+
+class ProductList(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+# class ProductList(APIView):
+#     def get(self, request, format=None):
+#         products = Product.objects.all()
+#         serializer = ProductSerializer(products, many=True)
+#         return Response(serializer.data)
 
 #old iteration of code 1
 
