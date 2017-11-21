4bc3afdbfb28d8741eda4d981c45bd590eb1415d
Tae Hong Min <taemin1@Taes-MacBook-Pro.local>
2017-11-21 18:07:11 -0500

    end of part 3

--- serverside/catalog/views.py
+++ serverside/catalog/views.py
@@ -1,72 +1,44 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Product, Review
+from .serializers import ProductSerializer, ReviewSerializer
 
-# Create your views here.
-from django.http import Http404
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework import status
 
 class ProductList(generics.ListCreateAPIView):
-	queryset = Product.objects.all()
-	serializer_class = ProductSerializer
-	permission_classes = (IsAdminOrReadOnly, )
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = (IsAdminOrReadOnly, )
+    lookup_url_kwarg = 'product_id'
+
 
 class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
-	queryset = Product.objects.all()
-	serializer_class = ProductSerializer
-	permission_classes = (IsAdminOrReadOnly, )
-
-# class ProductDetail(APIView):
-#     def get_object(self, pk):
-#         try:
-#             return Product.objects.get(pk=pk)
-#         except Product.DoesNotExist:
-#             raise Http404
-
-#     def get(self, request, pk, format=None):
-#         product = self.get_object(pk)
-#         serializer = ProductSerializer(product)
-#         return Response(serializer.data)
-
-#     def put(self, request, pk, format=None):
-#         product = self.get_object(pk)
-#         serializer = ProductSerializer(product, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-#     def delete(self, request, pk, format=None):
-#         product = self.get_object(pk)
-#         product.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
-
-# class ProductList(generics.ListAPIView):
-#     queryset = Product.objects.all()
-#     serializer_class = ProductSerializer
-
-# class ProductList(APIView):
-#     def get(self, request, format=None):
-#         products = Product.objects.all()
-#         serializer = ProductSerializer(products, many=True)
-#         return Response(serializer.data)
-
-#old iteration of code 1
-
-# class JSONResponse(HttpResponse):
-#     """
-#     An HttpResponse that renders its content into JSON.
-#     """
-#     def __init__(self, data, **kwargs):
-#         content = JSONRenderer().render(data)
-#         kwargs['content_type'] = 'application/json'
-#         super(JSONResponse, self).__init__(content, **kwargs)
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = (IsAdminOrReadOnly, )
+    lookup_url_kwarg = 'product_id'
 
 
-# def product_list(request):
-#     if request.method == 'GET':
-#         products = Product.objects.all()
-#         serializer = ProductSerializer(products, many=True)
-#         return JSONResponse(serializer.data)
+class ReviewList(generics.ListCreateAPIView):
+    serializer_class = ReviewSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly, )
+    lookup_url_kwarg = 'product_id'
 
-#Authorization: Token 33fc5e530746b4b54272eb7c3959a5caa150441c
+    def perform_create(self, serializer):
+        serializer.save(
+            created_by=self.request.user,
+            product_id=self.kwargs['product_id'])
+
+    def get_queryset(self):
+        product = self.kwargs['product_id']
+        return Review.objects.filter(product__id=product)
+
+
+class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ReviewSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
+    lookup_url_kwarg = 'review_id'
+
+    def get_queryset(self):
+        review = self.kwargs['review_id']
+        return Review.objects.filter(id=review)
